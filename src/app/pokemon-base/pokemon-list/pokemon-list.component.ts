014412a4f8805a626decffc568f4dbf1add8a661
teddysmithdev <teddysmithdev@gmail.com>
2022-08-09 08:03:23 -0400

    Added ViewChild()

--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 import { Pokemon } from 'src/app/models/pokemon';
 import { PokemonService } from 'src/app/_services/pokemon.service';
+import { PokemonDetailComponent } from '../pokemon-detail/pokemon-detail.component';
 //Decorator
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent implements OnInit, AfterViewInit {
   pokemons: Pokemon[] = [];
+  @ViewChildren('pokemonRef') pokemonRef!: ElementRef;
+
   constructor(private pokemonService : PokemonService) { 
+    console.log(this.pokemonRef);
     
+  }
+  ngAfterViewInit(): void {
+    console.log(this.pokemonRef)
   }
 
   handleRemove(event: Pokemon) {
