899f4626799fe31384c6b90b11da0b1961605875
teddysmithdev <teddysmithdev@gmail.com>
2022-08-13 10:37:59 -0400

    Add ElementRef and Renderer2

--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, AfterViewInit, Component, ContentChild, ContentChildren, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ContentChild, ContentChildren, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 import { Pokemon } from 'src/app/models/pokemon';
 import { PokemonService } from 'src/app/_services/pokemon.service';
@@ -12,9 +12,10 @@ import { PokemonDetailComponent } from '../pokemon-detail/pokemon-detail.compone
 export class PokemonListComponent implements OnInit, AfterViewInit, AfterContentInit {
   pokemons: Pokemon[] = [];
   @ViewChildren('pokemonRef') pokemonRef!: ElementRef;
+  @ViewChild('pokemonTh') pokemonTh!: ElementRef
   @ContentChildren(PokemonDetailComponent) contentList!: any;
 
-  constructor(private pokemonService : PokemonService) { 
+  constructor(private pokemonService : PokemonService, private renderer: Renderer2) { 
     console.log(this.pokemonRef);
     
   }
@@ -22,7 +23,12 @@ export class PokemonListComponent implements OnInit, AfterViewInit, AfterContent
     console.log(this.contentList);
   }
   ngAfterViewInit(): void {
-    console.log(this.pokemonRef)
+    console.log(this.pokemonTh)
+    this.pokemonTh.nativeElement.innerText = "Pokemon Name";
+    const div = this.renderer.createElement('div');
+    const text = this.renderer.createText('Hello world!');
+    this.renderer.appendChild(div, text);
+    this.renderer.appendChild(this.pokemonTh.nativeElement, div);
   }
 
   handleRemove(event: Pokemon) {
